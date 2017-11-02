3d7333aa4d49d5a4f93592b8d6c772ff298ae5d0
luiscastillo27 <luis@AirLuis.local>
2017-11-01 23:16:56 -0600

    models
    Se creo el modelo de usuario

---
+++ models/Usuarios.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "usuarios".
+ *
+ * @property integer $id
+ * @property string $usuario
+ * @property string $password
+ * @property integer $estado
+ * @property string $fecha
+ */
+class Usuarios extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'usuarios';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'usuario', 'password', 'estado', 'fecha'], 'required'],
+            [['id', 'estado'], 'integer'],
+            [['usuario', 'password'], 'string', 'max' => 27],
+            [['fecha'], 'string', 'max' => 11],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'usuario' => 'Usuario',
+            'password' => 'Password',
+            'estado' => 'Estado',
+            'fecha' => 'Fecha',
+        ];
+    }
+}
