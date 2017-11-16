a8d0c7e989b0e18dd76b6168e77467814d08ec18
luiscastillo27 <luisyosemite@gmail.com>
2017-11-16 11:11:19 -0600

    db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=capitell',
+    'dsn' => 'mysql:host=localhost;dbname=tiendayii',
     'username' => 'root',
     'password' => 'admin',
     'charset' => 'utf8',
