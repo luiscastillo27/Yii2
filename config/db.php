3aaec2069081aaf2dbc43fcead3b6d1aad5d74f8
luiscastillo27 <luisyosemite@gmail.com>
2017-11-16 11:09:20 -0600

    funcional

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tiendayii',
+    'dsn' => 'mysql:host=localhost;dbname=capitell',
     'username' => 'root',
     'password' => 'admin',
     'charset' => 'utf8',
